4c328f7473824cf496441b65149c5cb7b0e0900c
DESKTOP-26CSUBO\illia <illiazlobin@gmail.com>
2021-01-18 01:50:53 +0200

    Validation Exception

---
+++ src/core/exceptions/validation.exception.ts
@@ -0,0 +1,27 @@
+import { HttpException, HttpStatus } from '@nestjs/common';
+import { ValidationError } from '../interfaces';
+
+export class ValidationException extends HttpException {
+  errors: ValidationError[];
+
+  constructor(error: ValidationError);
+
+  constructor(errors: ValidationError[]);
+
+  constructor(message: string, property?: string[]);
+
+  constructor(
+    errorsOrMessage: ValidationError | ValidationError[] | string,
+    property: string[] = [],
+  ) {
+    super('The model is not valid', HttpStatus.BAD_REQUEST);
+
+    if (Array.isArray(errorsOrMessage)) {
+      this.errors = errorsOrMessage;
+    } else if (typeof errorsOrMessage === 'string') {
+      this.errors = [{ message: errorsOrMessage, property }];
+    } else {
+      this.errors = [errorsOrMessage];
+    }
+  }
+}
