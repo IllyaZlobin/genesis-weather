a4eaba99ef57910ca78eecd703e4f56590d4f069
DESKTOP-26CSUBO\illia <illiazlobin@gmail.com>
2021-01-18 16:44:37 +0200

    weather history service

---
+++ src/modules/weather/services/weatherHistory.service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { WeatherHistoryEntity } from '../../../typeorm';
+import { Repository } from 'typeorm';
+
+@Injectable()
+export class WeatherHistoryService {
+  constructor(
+    @InjectRepository(WeatherHistoryEntity)
+    private readonly weatherHistoryRepository: Repository<WeatherHistoryEntity>,
+  ) {}
+
+  async getMostSearchableCity(): Promise<{ city: string; total: number }> {
+    const [result] = (await this.weatherHistoryRepository
+      .createQueryBuilder('weather_history')
+      .select('COUNT(weather_history.city)', 'total')
+      .addSelect('weather_history.city', 'city')
+      .groupBy('weather_history.city')
+      .having('COUNT(weather_history.city) > 1')
+      .orderBy('total', 'DESC')
+      .execute()) as { city: string; total: number }[];
+
+    return result;
+  }
+}
