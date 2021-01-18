38bb60473bd1b816cc122334f73ca3338ce9057c
DESKTOP-26CSUBO\illia <illiazlobin@gmail.com>
2021-01-18 20:16:26 +0200

    added description for get all cities endpoint

--- src/modules/city/city.controller.ts
+++ src/modules/city/city.controller.ts
@@ -1,4 +1,5 @@
 import { Controller, Get } from '@nestjs/common';
+import { ApiOkResponse, ApiOperation } from '@nestjs/swagger';
 import { GetAllCitiesResponse } from './dto/getAll/getAll.response';
 import { CityService } from './services/city.service';
 
@@ -7,6 +8,8 @@ export class CityController {
   constructor(private readonly cityService: CityService) {}
 
   @Get()
+  @ApiOkResponse({ type: GetAllCitiesResponse })
+  @ApiOperation({ summary: 'Return list of available city names' })
   async getAllAvailable() {
     const cities = await this.cityService.getAvailable();
     const response = new GetAllCitiesResponse(cities);
