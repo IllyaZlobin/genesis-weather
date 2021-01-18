2529cfc84a187ea2d66e83f6dc4434fcf05f01b6
DESKTOP-26CSUBO\illia <illiazlobin@gmail.com>
2021-01-18 16:45:14 +0200

    added cross env and app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
+import { ScheduleModule } from '@nestjs/schedule';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { SharedModule } from './modules';
+import { CityModule } from './modules/city/city.module';
+import { WeatherModule } from './modules/weather/weather.module';
 
 @Module({
-  imports: [SharedModule],
+  imports: [ScheduleModule.forRoot(), SharedModule, CityModule, WeatherModule],
   controllers: [AppController],
   providers: [AppService],
 })
