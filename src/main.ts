de283e913a5fee4cb341a6f51e09dc3ac22b95c7
DESKTOP-26CSUBO\illia <illiazlobin@gmail.com>
2021-01-18 16:25:19 +0200

    added port from env

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from './modules';
 import {
   setupFilters,
   setupInterceptors,
@@ -9,11 +10,17 @@ import {
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const config = app.get(ConfigService);
 
   setupPipes(app);
   setupFilters(app);
   setupInterceptors(app);
   setupSwagger(app, 'Genesis. Weather API', 'Apis', '1.0.0');
+
+  const port = config.get('PORT') || 3000;
+  const env = config.nodeEnv;
+
+  console.log(`APP RUN ON ${port}, ENV: ${env}`);
 
   await app.listen(3000);
 }
